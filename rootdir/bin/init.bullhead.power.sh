9c84aa82b47ac963b40e0ed3ce46e8a3a94d9103
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-08 17:10:25 -0700

    bullhead4c: rootdir: Gut A57 cores from power.sh
    * We only want to use the A53 (LITTLE) cores
    
    Change-Id: I156135c5c3f5073d0a71ba3ac87c9ed137e91461

--- rootdir/bin/init.bullhead.power.sh
+++ rootdir/bin/init.bullhead.power.sh
@@ -20,7 +20,7 @@ function get-set-forall() {
 
 ################################################################################
 
-# take the A57s offline when thermal hotplug is disabled
+# Ensure the A57s are offline (they should be from the kernel cmdline)
 write /sys/devices/system/cpu/cpu4/online 0
 write /sys/devices/system/cpu/cpu5/online 0
 
@@ -35,12 +35,6 @@ get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
 # /sys/. These files receive the default label "sysfs".
 # Restorecon again to give new files the correct label.
 restorecon -R /sys/devices/system/cpu
-
-# Best effort limiting for first time boot if msm_performance module is absent
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
-
-# Limit A57 max freq from msm_perf module in case CPU 4 is offline
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000"
 
 # Disable CPU retention
 write /sys/module/lpm_levels/system/a53/cpu0/retention/idle_enabled 0
@@ -68,33 +62,6 @@ write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:7
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
-
-# online CPU4
-write /sys/devices/system/cpu/cpu4/online 1
-
-# configure governor settings for big cluster
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
-restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 633600
-
-# restore A57's max
-copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-
-# plugin remaining A57s
-write /sys/devices/system/cpu/cpu5/online 1
-
-# Restore CPU 4 max freq from msm_performance
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295"
 
 # input boost configuration
 write /sys/module/cpu_boost/parameters/input_boost_freq "0:960000"
