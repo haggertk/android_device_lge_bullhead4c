9406fa76379ca24da22c8476edf994d82b3ede3a
Paul Keith <javelinanddart@gmail.com>
2018-03-09 16:27:52 +0100

    bullhead4c: Update interactive and scheduler parameters
    * Kanged from LineageOS/android_device_cyanogen_msm8916-common@4c2c915

--- rootdir/bin/init.bullhead.power.sh
+++ rootdir/bin/init.bullhead.power.sh
@@ -51,16 +51,14 @@ write /sys/module/lpm_levels/system/a57/a57-l2-retention/idle_enabled 0
 # configure governor settings for little cluster
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "25000 1094400:50000"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 90
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 30000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 998400
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "1 800000:85 998400:90 1094400:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 50000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 50000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
 
 # input boost configuration
@@ -68,22 +66,16 @@ write /sys/module/cpu_boost/parameters/input_boost_freq "0:960000"
 write /sys/module/cpu_boost/parameters/input_boost_ms 40
 
 # Setting B.L scheduler parameters
-write /proc/sys/kernel/sched_migration_fixup 1
-write /proc/sys/kernel/sched_upmigrate 95
-write /proc/sys/kernel/sched_downmigrate 85
-write /proc/sys/kernel/sched_freq_inc_notify 400000
-write /proc/sys/kernel/sched_freq_dec_notify 400000
+write /proc/sys/kernel/sched_window_stats_policy 3
+write /proc/sys/kernel/sched_ravg_hist_size 3
+write /proc/sys/kernel/sched_small_task 20
+write /proc/sys/kernel/sched_mostly_idle_load 30
+write /proc/sys/kernel/sched_mostly_idle_nr_run 3
 
 #enable rps static configuration
 write /sys/class/net/rmnet_ipa0/queues/rx-0/rps_cpus 8
 
-# android background processes are set to nice 10. Never schedule these on the a57s.
-write /proc/sys/kernel/sched_upmigrate_min_nice 9
-
 get-set-forall  /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
-
-# Disable sched_boost
-write /proc/sys/kernel/sched_boost 0
 
 # re-enable thermal and BCL hotplug
 write /sys/module/msm_thermal/core_control/enabled 1
