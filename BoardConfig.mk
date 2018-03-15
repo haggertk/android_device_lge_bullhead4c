eaee70212b0148d7529bb9fb79ae18af66500476
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-15 04:14:28 -0600

    bullhead4c: Use our custom power HAL
    Change-Id: I3d26c55b14f42465cccdab14087550b0c7af4b89

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,3 +23,5 @@ TARGET_OTA_ASSERT_DEVICE := bullhead,bullhead4c
 # Kernel (only use the Cortex-A53 cores at boot)
 BOARD_KERNEL_CMDLINE := $(subst boot_cpus=0-5,boot_cpus=0-3,$(BOARD_KERNEL_CMDLINE))
 BOARD_KERNEL_CMDLINE += maxcpus=4
+
+TARGET_POWERHAL_VARIANT := bullhead4c
